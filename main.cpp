288ac37c00d805d616dd9d1fa1d07e73b9f54189
Kyle Van Meter <kvanmete76751@gmail.com>
2021-01-25 04:35:09 -0700

    main: Implement class View, Redo SDL usage
    We are now correctly sending a buffer to a texture (I think on the GPU?) instead
    of inefficiently coloring NUM_SQUARES SDL_Rects.
    
    Still not 100% certain on the timing method for the emulator.  It appears to be
    causing some flickering, and the values that should be for 60Hz are way too slow
    so something must be off...

--- main.cpp
+++ main.cpp
@@ -1,64 +1,108 @@
 #include "chip8.h"
 
 #include <chrono>
+#include <cstring>
 #include <iostream>
 
 #include <SDL2/SDL.h>
 
-#define HEIGHT 32 * 10
-#define WIDTH 64 * 10
+#define HEIGHT 32
+#define WIDTH 64
 #define NUM_SQUARES 2048
+
+class View {
+public:
+  View(const std::string &title, int width, int height, int scale = 1) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+      throw std::runtime_error(SDL_GetError());
+    }
+
+    _window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED,
+                               SDL_WINDOWPOS_CENTERED, width * scale,
+                               height * scale, 0);
+    _render = SDL_CreateRenderer(_window, -1, 0);
+    _texture = SDL_CreateTexture(_render, SDL_PIXELFORMAT_RGB24,
+                                 SDL_TEXTUREACCESS_STREAMING, width, height);
+
+    if (!_window) {
+      throw std::runtime_error(SDL_GetError());
+    }
+    if (!_render) {
+      throw std::runtime_error(SDL_GetError());
+    }
+    if (!_texture) {
+      throw std::runtime_error(SDL_GetError());
+    }
+  };
+
+  ~View() {
+    SDL_DestroyTexture(_texture);
+    SDL_DestroyRenderer(_render);
+    SDL_DestroyWindow(_window);
+    SDL_Quit();
+  };
+
+  void SetFrame(unsigned char *data, int height) {
+    void *pixeldata;
+    int width;
+
+    SDL_LockTexture(_texture, nullptr, &pixeldata, &width);
+    std::memcpy(pixeldata, data, width * height);
+    SDL_UnlockTexture(_texture);
+  };
+
+  void CopyToRGB24(unsigned char *in, unsigned char *data) {
+    for (int i = 0; i < NUM_SQUARES; i++) {
+      if (in[i] != 0) {
+        data[i * 3] = 255;
+        data[i * 3 + 1] = 0;
+        data[i * 3 + 2] = 0;
+      } else {
+        data[i * 3] = 0;
+        data[i * 3 + 1] = 0;
+        data[i * 3 + 2] = 0;
+      }
+    }
+  }
+
+  void Update() {
+    SDL_RenderCopy(_render, _texture, NULL, NULL);
+    SDL_RenderPresent(_render);
+  };
+
+private:
+  SDL_Window *_window;
+  SDL_Renderer *_render;
+  SDL_Texture *_texture;
+};
 
 chip8 Chip;
 
-void setupInput() {
-  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-    std::cerr << "SDL_Init Error: " << SDL_GetError() << std::endl;
-    std::exit(-1);
-  }
-}
-
-void run(SDL_Window *window, SDL_Renderer *render) {
+int main(int argc, char **argv) {
   std::chrono::time_point<std::chrono::steady_clock> begin, end;
 
-  // Setup each pixel as a rect so our display isn't horribly small.
-  SDL_Rect rects[NUM_SQUARES];
-  int iH = HEIGHT / 32;
-  int iW = WIDTH / 32;
-  int x = WIDTH / 2 - iW / 2;
-  int y = HEIGHT / 2 - iH / 2;
-  for (int i = 0; i < NUM_SQUARES; i++) {
-    rects[i].x = i / 32 * iH;
-    rects[i].y = i % 32 * iW / 2;
-    rects[i].w = iW / 2;
-    rects[i].h = iH;
-  }
+  // Clear chip8 memory, and copy program into memory
+  Chip.init();
+  Chip.load();
+
+  View viewer("Chip8", WIDTH, HEIGHT, 10);
+  unsigned char *rgb = static_cast<unsigned char *>(
+      std::calloc(NUM_SQUARES * 3, sizeof(unsigned char)));
+
+  viewer.SetFrame(rgb, HEIGHT);
 
   try {
     for (;;) {
       begin = std::chrono::steady_clock::now();
       Chip.emuCycle();
 
-      // This flag is only set by two opcodes
-      // 0x00E0 - Clear screen
-      // 0xDXYN - Draw sprite
       if (Chip.drawFlag) {
-        for (int i = 0; i < NUM_SQUARES; i++) {
-          if (Chip.gfx[i]) {
-            SDL_SetRenderDrawColor(render, 255, 255, 255, 255);
-          } else {
-            SDL_SetRenderDrawColor(render, 10, 10, 10, 255);
-          }
+        viewer.CopyToRGB24(Chip.gfx, rgb);
+        viewer.SetFrame(rgb, HEIGHT);
+        viewer.Update();
 
-          SDL_RenderFillRect(render, &rects[i]);
-        }
-        SDL_RenderPresent(render);
-        SDL_RenderClear(render);
         Chip.drawFlag = false;
-        // drawGraphics();
       }
-
-      end = std::chrono::steady_clock::now();
 
       // TODO: Clean this up for obvious reasons!
       // 16 is approximately 1/60, or 60 Hz in ms.
@@ -66,36 +110,18 @@ void run(SDL_Window *window, SDL_Renderer *render) {
       // should still execute the loop at least once.  Basically we are trying
       // to wait until at least 1/60th of a second has passed for each emulation
       // cycle for usability / accuracy.
+      end = std::chrono::steady_clock::now();
       do {
-        // Store key states for each cycle
         Chip.setKeys();
         end = std::chrono::steady_clock::now();
       } while (
           std::chrono::duration_cast<std::chrono::milliseconds>(end - begin)
-              .count() <= 10);
+              .count() <= 2);
     }
   } catch (std::exception &e) {
     std::cerr << "ERR: " << e.what();
     std::exit(-1);
   }
-}
-int main(int argc, char **argv) {
-  setupInput();
 
-  // SDL2 init stuff goes here
-  SDL_Window *window =
-      SDL_CreateWindow("Chip8", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-                       WIDTH, HEIGHT, 0);
-  SDL_Renderer *render = SDL_CreateRenderer(window, -1, 0);
-
-  // Clear chip8 memory, and copy program into memory
-  Chip.init();
-  Chip.load();
-
-  run(window, render);
-
-  SDL_DestroyRenderer(render);
-  SDL_DestroyWindow(window);
-  SDL_Quit();
   return 0;
 }
