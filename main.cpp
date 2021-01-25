2b2260bd0abb0420c9cbfd36922a6901aad3eb0b
Kyle Van Meter <kvanmete76751@gmail.com>
2021-01-25 16:10:29 -0700

    Makefile, view: Move View class into its own file
    Out of main for compilation speed, and readability.

--- main.cpp
+++ main.cpp
@@ -1,80 +1,13 @@
-#include "chip8.h"
-
 #include <chrono>
 #include <cstring>
 #include <iostream>
 
-#include <SDL2/SDL.h>
+#include "chip8.h"
+#include "view.h"
 
 #define HEIGHT 32
 #define WIDTH 64
 #define NUM_SQUARES 2048
-
-class View {
-public:
-  View(const std::string &title, int width, int height, int scale = 1) {
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-      throw std::runtime_error(SDL_GetError());
-    }
-
-    _window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED,
-                               SDL_WINDOWPOS_CENTERED, width * scale,
-                               height * scale, 0);
-    _render = SDL_CreateRenderer(_window, -1, 0);
-    _texture = SDL_CreateTexture(_render, SDL_PIXELFORMAT_RGB24,
-                                 SDL_TEXTUREACCESS_STREAMING, width, height);
-
-    if (!_window) {
-      throw std::runtime_error(SDL_GetError());
-    }
-    if (!_render) {
-      throw std::runtime_error(SDL_GetError());
-    }
-    if (!_texture) {
-      throw std::runtime_error(SDL_GetError());
-    }
-  };
-
-  ~View() {
-    SDL_DestroyTexture(_texture);
-    SDL_DestroyRenderer(_render);
-    SDL_DestroyWindow(_window);
-    SDL_Quit();
-  };
-
-  void SetFrame(unsigned char *data, int height) {
-    void *pixeldata;
-    int width;
-
-    SDL_LockTexture(_texture, nullptr, &pixeldata, &width);
-    std::memcpy(pixeldata, data, width * height);
-    SDL_UnlockTexture(_texture);
-  };
-
-  void CopyToRGB24(unsigned char *in, unsigned char *data) {
-    for (int i = 0; i < NUM_SQUARES; i++) {
-      if (in[i] != 0) {
-        data[i * 3] = 255;
-        data[i * 3 + 1] = 0;
-        data[i * 3 + 2] = 0;
-      } else {
-        data[i * 3] = 0;
-        data[i * 3 + 1] = 0;
-        data[i * 3 + 2] = 0;
-      }
-    }
-  }
-
-  void Update() {
-    SDL_RenderCopy(_render, _texture, NULL, NULL);
-    SDL_RenderPresent(_render);
-  };
-
-private:
-  SDL_Window *_window;
-  SDL_Renderer *_render;
-  SDL_Texture *_texture;
-};
 
 chip8 Chip;
 
