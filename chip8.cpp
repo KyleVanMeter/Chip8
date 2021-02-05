5875587e98d5457ccc73fc97a99e3c321ae24e72
Kyle Van Meter <kvanmete76751@gmail.com>
2021-02-04 17:50:19 -0700

    chip8.cpp: OP_8XY3, OP_8XYE

--- chip8.cpp
+++ chip8.cpp
@@ -179,7 +179,12 @@ void chip8::emuCycle() {
       PC += 2;
       break;
     case Opcodes::Chip8::OP_8XY3:
-      std::cerr << "Hit opcode " << std::hex << opcode << "\n";
+      std::cout << PC << " Set V[" << ((opcode & 0x0F00) >> 8) << "] to "
+                << " V[" << ((opcode & 0x0F00) >> 8) << "] ^ V["
+                << ((opcode & 0x00F0) >> 4) << "]\n";
+
+      V[(opcode & 0x0F00) >> 8] ^= V[(opcode & 0x00F0) >> 4];
+      PC += 2;
       break;
     case Opcodes::Chip8::OP_8XY4: {
       std::cout << PC << " Set V[" << ((opcode & 0x0F00) >> 8) << "] to V["
@@ -234,7 +239,12 @@ void chip8::emuCycle() {
       PC += 2;
       break;
     case Opcodes::Chip8::OP_8XYE:
-      std::cerr << "Hit opcode " << std::hex << opcode << "\n";
+      std::cout << PC << " Set V[0xF] to MSB of V[" << ((opcode & 0x0F00) >> 8)
+                << ", then multiply it by 2.\n";
+
+      V[0xF] = V[((opcode & 0x0F00) >> 8)] > 0x80;
+      V[(opcode & 0x0F00) >> 8] <<= 1;
+      PC += 2;
       break;
     default:
       throw std::runtime_error("Unknown opcode in 8: " + intToString(opcode) +
